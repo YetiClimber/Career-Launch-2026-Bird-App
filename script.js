7a79d9e611b23f669888b0bdc551ec70bf209ed9
Lizzy <lizzydenisova9529@gmail.com>
2026-01-22 16:48:59 -0500

    Refactor script.js for profile management and authentication

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+  document.addEventListener("DOMContentLoaded", () => {
+
+
+    
     var username = "birdQuestTest";
     var password = "abc123";
-    var birdnum = 0;
+
+    var pfp = document.getElementById("upload");
+    var newPfp = document.getElementById("pic");
+    var miniPfp = document.getElementById("mini-pfp");
+    var savedPic = localStorage.getItem("profilePic");
+    var welcome = document.getElementById("output-welcome");
+
+    var displayNameInput = document.getElementById("display-name");
+    var saveAcc = document.getElementById("save");
     
+    // index.html function - hardcoded login and password
     function authenticate() {
       var authorized;
 
@@ -14,10 +27,61 @@
         authorized = true;
       } else {
         authorized = false;
-        alert("Username or password  is incorrect.")
+        alert("Username or password is incorrect.")
       }
       return authorized;
     }
 
-    document.getElementById("output_welcome").textContent = "Welcome, " + username + "!";
-    document.getElementById("output_birdnum").textContent = "You have discovered " + birdnum + " birds so far. Neat!";
+    // homePage.html information
+    if (displayNameInput && saveAcc) {
+      const savedName = localStorage.getItem("displayName");
+      if (savedName){
+        displayNameInput.value = savedName;
+      }
+
+      saveAcc.addEventListener("click", () => {
+        var name = displayNameInput.value.trim();
+        if (name !== "") {
+          localStorage.setItem("displayName", name);
+          alert("Saved Changes!");
+        }
+      });
+    }
+
+    if (welcome) {
+      const savedName = localStorage.getItem("displayName");
+      welcome.textContent = savedName || "User";
+    }
+
+
+    
+
+    if (pfp && newPfp) {
+    // account.html profile change
+      pfp.addEventListener("change", function () {
+        const file = this.files[0];
+        if (!file) return;
+
+        const reader = new FileReader();
+
+        reader.onload = function () {
+          newPfp.src = reader.result;
+          localStorage.setItem("profilePic", reader.result);
+        };
+
+        reader.readAsDataURL(file);
+      });
+    }
+
+    if (miniPfp && savedPic) {
+      miniPfp.src = savedPic;
+    }
+
+    if (newPfp && savedPic) {
+      newPfp.src = savedPic;
+    }
+});
+
+
+
+
